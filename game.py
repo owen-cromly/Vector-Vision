aee9429bae4b7cf5dcb7cd1bec9ae5ddc20c90da
owen-cromly <98927983+owen-cromly@users.noreply.github.com>
2023-08-19 22:40:07 -0400

    improvements, fixes

--- game.py
+++ game.py
@@ -7,53 +7,65 @@ class MyGame(Tk):
     '''
     This class represents the game itself. It has a window with several kinds of frames.
     '''
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, bg='sky blue', **kwargs):
         ctypes.windll.shcore.SetProcessDpiAwareness(True)
         Tk.__init__(self, *args, **kwargs)
         bin = Frame(self)
+        bin.pack(side = "top", fill = "both", expand = True)
         self.geometry("1500x1200")
         self.title("Vector Vision - Primitives")
-        bin.pack(fill="both",expand=True)
-        self.main = self.MainMenu(bin, controller = self)
-        self.help = self.HelpMenu(bin, controller = self)
-        self.gameplay = self.Gameplay(bin, controller = self)
-        self.main.grid(x = 0, y = 0, sticky = "nsew")
-        self.help.grid(x = 0, y = 0, sticky = "nsew")
-        self.gameplay.grid(x = 0, y = 0, sticky = "nsew")
-        self.frames = {self.main.__name__: self.main, self.help.__name__: self.help, self.gameplay.__name__: self.gameplay}
+        self.bg = bg
+        self.gameRunning = False
+        self.main = self.MainMenu(bin, self, bg)
+        self.help = self.HelpMenu(bin, self, bg)
+        self.gameplay = self.Gameplay(bin, self, bg)
+        self.main.grid(row = 0, column = 0, sticky = "nsew")
+        self.help.grid(row = 0, column = 0, sticky = "nsew")
+        self.gameplay.grid(row = 0, column = 0, sticky = "nsew")
+        self.frames = {"MainMenu": self.main, "HelpMenu": self.help, "Gameplay": self.gameplay}
+        bin.grid_rowconfigure(0, weight=1)
+        bin.grid_columnconfigure(0, weight=1)
+        self.showFrame("MainMenu")
+        self.bind("<KeyPress>", self.gameplay.key_down)
+        self.bind("<KeyRelease>", self.gameplay.key_up)
+        self.refresh()
 
     def showFrame(self, frame):
+        self.gameplay.guts.act([], self.gameplay.canvas)
         self.frames[frame].tkraise()
 
-    # this is a test edit of no consequence
-    
-    # random edit that has no effect
-
-    # yet another edit with no effect
+    def refresh(self):
+        self.gameplay.respond()
+        if self.gameRunning:
+            self.after(16, self.refresh)
 
     class MainMenu(Frame):
         '''
         This class represents the opening menu of the game
         '''
-        def __init__(self, parent, controller):
+        def __init__(self, parent, controller, bg):
             Frame.__init__(self, parent)
             self.controller = controller
-            titleMessage = Label(self, text = "Welcome to Vector Vision!", font = Font(family = "Comic Sans", size = 20, weight = "bold"))
+            titleMessage = Label(self, text = "Welcome to Vector Vision!", font = Font(family = "Helvetica", size = 40, weight = "bold"))
             titleMessage.pack(pady = 10)
-            description = Label(self, text = "Use WASD to move and arrow keys to look around \n Press I to enter editor mode, where:\n -->new line from/to existing point: Q \n --> new line from/to new point: R \n Press M to switch movement modes (not yet a feature) \n Press H for help", font = "Comic Sans")
-            description.pack(pady = 10)
-            button = Button(self, text = "Play", command = self.play)
+            description = Label(self, text = "Use WASD to move and arrow keys to look around \n\n Press [I] to enter editor mode, where you can add lines and vertices: \n-->[Q] connects a new line to an existing point; \n-->[R] creates a new point; \n\n Press M to switch movement modes (not yet a feature) \n\n Press H for help", font = Font(size = 15, family = "Helvetica"))
+            description.pack(pady = 60)
+            button = Button(self, text = "Play", width = 20, height = 5, command = self.play)
             button.pack()
+            self.config(bg=bg)
 
         def play(self):
             self.controller.showFrame("Gameplay")
 
+        def tkraise(self, *args, **kwargs):
+            Frame.tkraise(self, *args, **kwargs)
+            self.controller.gameRunning = False
     
     class HelpMenu(Frame):
         '''
         This class represents a help screen
         '''
-        def __init__(self, parent, controller):
+        def __init__(self, parent, controller, bg):
             Frame.__init__(self, parent)
             self.controller = controller
             titleMessage = Label(self, text = "Help Screen", font = Font(family = "Comic Sans", size = 20, weight = "bold"))
@@ -66,11 +78,15 @@ class MyGame(Tk):
         def play(self):
             self.controller.showFrame("Gameplay")
 
+        def tkraise(self, *args, **kwargs):
+            Frame.tkraise(self, *args, **kwargs)
+            self.controller.gameRunning = False
+
     class Gameplay(Frame):
         '''
         This class represents the actual gameplay
         '''
-        def __init__(self, parent, controller):
+        def __init__(self, parent, controller, bg):
             Frame.__init__(self, parent)
             self.controller = controller
             self.canvas = Canvas(self)
@@ -82,12 +98,8 @@ class MyGame(Tk):
                                 [1,3,5,7,4,6,2,0,5,7,3,1,9,10,11,13,14,14,15,15,15]], int32))
 
             self.buttons = []
-
-            self.bind("<KeyPress>", self.key_down)
-            self.bind("<KeyRelease>", self.key_up)
-            self.config(bg='sky blue')
-            self.pack(fill="both",expand=True)
-            self.respond()
+            self.canvas.config(bg=bg)
+            self.canvas.pack(fill="both", expand=True)
 
         def key_down(self, event):
             if not event.keycode in self.buttons:
@@ -103,12 +115,14 @@ class MyGame(Tk):
         def respond(self):
             if self.buttons:
                 self.guts.act(self.buttons, self.canvas)
-            self.after(16, self.respond)
 
         def help(self):
             self.controller.showFrame("HelpMenu")
 
-        respond()
+        def tkraise(self,*args,**kwargs):
+            Frame.tkraise(self, *args, **kwargs)
+            self.controller.gameRunning = True
+            self.controller.refresh()
 
         
 if __name__ == "__main__":
